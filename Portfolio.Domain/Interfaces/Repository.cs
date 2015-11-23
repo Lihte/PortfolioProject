c82de05660a7ce511ab29f7b1bbe5626331f8ab0
Lihte <ful_gnagare@hotmail.com>
2015-11-23 19:21:08 +0100

    Basic Setup

---
+++ Portfolio.Domain/Interfaces/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Domain.Interfaces
+{
+    public interface Repository<T>
+    {
+        void Add(T entity);
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        T Update(T entity);
+        void Delete(T entity);
+    }
+}
